2a1d2b8504dffc9acce7faeff57b5cdf89406d00
abhinayaanair <abhinayapersonal11@gmail.com>
2025-01-16 22:38:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from app.__init__ import create_app
+
+
+
+print("Current working directory:", os.getcwd())  # Print current working directory
+
+# Create an instance of the Flask application
+app = create_app()
+
+if __name__ == '__main__':
+    print("Flask app is running on http://127.0.0.1:5000/")
+    port = int(os.environ.get('PORT', 5000))  # Get the port from environment variable
+    app.run(host='0.0.0.0', port=port, debug=True)
